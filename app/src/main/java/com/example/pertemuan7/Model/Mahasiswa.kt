2cff26d8f17baa6af39b7534af569127f5324bbe
arifrahmadi23 <muhammad231arif@gmail.com>
2024-11-18 21:02:29 +0700

    Menambahkan variable noHp pada data class Mahasiswa

--- app/src/main/java/com/example/pertemuan7/Model/Mahasiswa.kt
+++ app/src/main/java/com/example/pertemuan7/Model/Mahasiswa.kt
@@ -6,5 +6,6 @@ data class Mahasiswa(
     val nama : String ="",
     val gender : String ="",
     val alamat : String ="",
-    val email : String =""
+    val email : String ="",
+    val noHp : String = ""
 )
